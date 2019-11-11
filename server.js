8253358bdd766bbbfab85f67c85aefa0f86911b9
petrus <petrus@g2academy.co>
2019-11-11 22:23:11 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var mongoose = require('mongoose')
+
+var app = express()
+
+mongoose.connect('mongodb+srv://test:test@cluster0-2emxt.mongodb.net/test?retryWrites=true&w=majority',function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log("sudah")
+    }
+})
+
+
+
+
+app.get('/', function(req, res, next){
+    res.json("Hallo selamat datang")
+})
+
+app.get('/:name', function(req, res, name){
+    res.json(req.params.name)
+})
+
+
+
+
+app.listen(3000, function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log("app ke port 3000")
+    }
+})
